79d259c001715686928fa57ae63db8dafe16e490
PayUp Mac Pro <PayUpmacpro@macs-MacBook-Pro.local>
2021-05-08 15:35:34 +0400

    added routing

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, history, match, linkUrl }) => {
+  console.log(match, linkUrl);
+  console.log(linkUrl);
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className="menu-item"
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
